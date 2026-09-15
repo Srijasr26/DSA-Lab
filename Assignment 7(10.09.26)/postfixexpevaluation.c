dd1a7d17e68ec5cebc71dbbb6091ce564bec6608
Srijasr26 <rsrija891@gmail.com>
2026-09-16 00:38:56 +0530

    Add files via upload

---
+++ Assignment 7(10.09.26)/postfixexpevaluation.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <ctype.h>
+
+#define MAX 100
+
+int stack[MAX];
+int top = -1;
+
+// Push element into stack
+void push(int value) {
+    stack[++top] = value;
+}
+
+// Pop element from stack
+int pop() {
+    return stack[top--];
+}
+
+int main() {
+    char postfix[MAX];
+    int i, a, b, result;
+
+    printf("Enter postfix expression: ");
+    scanf("%s", postfix);
+
+    for (i = 0; postfix[i] != '\0'; i++) {
+
+        // If operand, push it
+        if (isdigit(postfix[i])) {
+            push(postfix[i] - '0');
+        }
+
+        // If operator, perform operation
+        else {
+            b = pop();
+            a = pop();
+
+            switch (postfix[i]) {
+                case '+':
+                    result = a + b;
+                    break;
+
+                case '-':
+                    result = a - b;
+                    break;
+
+                case '*':
+                    result = a * b;
+                    break;
+
+                case '/':
+                    result = a / b;
+                    break;
+
+                default:
+                    printf("Invalid operator\n");
+                    return 1;
+            }
+
+            push(result);
+        }
+    }
+
+    printf("Result = %d\n", pop());
+
+    return 0;
+}
